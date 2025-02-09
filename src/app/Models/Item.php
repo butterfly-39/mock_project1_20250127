fc3da676e1d97d56260b361afc991c041b78d167
butterfly <serenitaey39@gmail.com>
2025-02-09 15:48:31 +0900

    テーブルの作成

---
+++ src/app/Models/Item.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'condition_id',
+        'image_url',
+        'name',
+        'description',
+        'price',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function condition()
+    {
+        return $this->belongsTo(Condition::class);
+    }
+}
