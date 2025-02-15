06710fc1fc1c831b63719364f23e53d27da48236
butterfly <serenitaey39@gmail.com>
2025-02-15 15:43:36 +0900

    CommentControllerの作成

--- src/app/Http/Controllers/CommentCotroller.php
+++ src/app/Http/Controllers/CommentCotroller.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function item_comment_create($item_id)
+    {
+        return view('profiles.comment', ['item_id' => $item_id]);
+    }
 }
+
