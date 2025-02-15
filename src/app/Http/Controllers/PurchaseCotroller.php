461acf7a256a80f305e4137da3ab513f285ee0f1
butterfly <serenitaey39@gmail.com>
2025-02-15 15:41:16 +0900

    PurchaseControllerの作成

--- src/app/Http/Controllers/PurchaseCotroller.php
+++ src/app/Http/Controllers/PurchaseCotroller.php
@@ -4,7 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PurchaseCotroller extends Controller
+class PurchaseController extends Controller
 {
-    //
+    public function purchase_view($item_id)
+    {
+        return view('profiles.purchase', ['item_id' => $item_id]);
+    }
+
+    public function purchase_update($item_id)
+    {
+        return view('profiles.purchase', ['item_id' => $item_id]);
+    }
+
+    public function address_view($item_id)
+    {
+        return view('profiles.address', ['item_id' => $item_id]);
+    }
+
+    public function address_update($item_id)
+    {
+        return view('profiles.address', ['item_id' => $item_id]);
+    }
+    
 }
+
