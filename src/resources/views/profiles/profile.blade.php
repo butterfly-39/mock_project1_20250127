059048ceb48529e5c6df96c2fe287b9643fcfe3a
butterfly <serenitaey39@gmail.com>
2025-03-10 19:32:58 +0900

    プロフィールデータの取得

--- src/resources/views/profiles/profile.blade.php
+++ src/resources/views/profiles/profile.blade.php
@@ -13,8 +13,8 @@
             <div class="profile-form__group">
                 <div class="profile-form__image-container">
                     <div class="profile-form__image-preview" id="preview">
-                        @if(isset($user->image))
-                            <img src="{{ asset('storage/' . $user->image) }}" alt="プロフィール画像">
+                        @if(isset($user->profile->image))
+                            <img src="{{ asset('storage/' . $user->profile->image) }}" alt="プロフィール画像">
                         @else
                             <div class="profile-form__no-image">
                                 <i class="fas fa-user"></i>
@@ -38,7 +38,7 @@
 
             <div class="profile-form__group">
                 <label class="profile-form__label" for="postal_code">郵便番号</label>
-                <input class="profile-form__input" type="text" name="postal_code" id="postal_code" value="{{ old('postal_code') }}">
+                <input class="profile-form__input" type="text" name="postal_code" id="postal_code" value="{{ old('postal_code', $user->profile->postal_code) }}">
                 <p class="profile-form__error-message">
                     @error('postal_code')
                     {{ $message }}
@@ -48,7 +48,7 @@
 
             <div class="profile-form__group">
                 <label class="profile-form__label" for="address">住所</label>
-                <input class="profile-form__input" type="text" name="address" id="address" value="{{ old('address') }}">
+                <input class="profile-form__input" type="text" name="address" id="address" value="{{ old('address', $user->profile->address) }}">
                 <p class="profile-form__error-message">
                     @error('address')
                     {{ $message }}
@@ -58,7 +58,7 @@
 
             <div class="profile-form__group">
                 <label class="profile-form__label" for="building">建物名</label>
-                <input class="profile-form__input" type="text" name="building" id="building" value="{{ old('building') }}">
+                <input class="profile-form__input" type="text" name="building" id="building" value="{{ old('building', $user->profile->building) }}">
             </div>
 
             <div class="profile-form__btn-group">
