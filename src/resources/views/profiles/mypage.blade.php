e7c0d461047955802914113af06c994c66fcee1f
butterfly <serenitaey39@gmail.com>
2025-03-22 02:35:18 +0900

    マイページの修正

--- src/resources/views/profiles/mypage.blade.php
+++ src/resources/views/profiles/mypage.blade.php
@@ -20,8 +20,8 @@
     </div>
 
     <div class="mypage-form__group">
-        <a href="/mypage?tab=sell" class="mypage-form__heading {{ request()->get('tab') === 'sell' ? 'active' : '' }}">出品した商品</a>
-        <a href="/mypage?tab=buy" class="mypage-form__heading {{ request()->get('tab') === 'buy' ? 'active' : '' }}">購入した商品</a>
+        <a href="/mypage?tab=sell" class="mypage-form__heading {{ request()->get('tab', 'sell') === 'sell' ? 'active' : '' }}">出品した商品</a>
+        <a href="/mypage?tab=buy" class="mypage-form__heading {{ request()->get('tab', 'sell') === 'buy' ? 'active' : '' }}">購入した商品</a>
     </div>
 </div>
 
