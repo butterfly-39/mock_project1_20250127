1b02b74cfa9a8b73d09bf36a79e169fad9fc122b
butterfly <serenitaey39@gmail.com>
2025-03-23 16:10:23 +0900

    データベースの修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategoriesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(ItemsTableSeeder::class);
+        $this->call(ItemCategoriesTableSeeder::class);
     }
 }
