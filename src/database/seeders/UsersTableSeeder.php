4c8c9d9338da20b2b491a9dc40aa78cb6a032f92
butterfly <serenitaey39@gmail.com>
2025-03-23 17:06:49 +0900

    データベースの修正

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,11 +15,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $params = [
+        // ユーザーを作成
+        $userId = DB::table('users')->insertGetId([
             'name' => 'test',
             'email' => 'test@test.com',
-            'password' => 'password',
-        ];
-        DB::table('users')->insert($params);
+            'password' => Hash::make('password'),
+        ]);
+
+        // プロフィールを作成
+        DB::table('profiles')->insert([
+            'user_id' => $userId,
+            'postal_code' => '123-4567',
+            'address' => '東京都渋谷区代々木1-1-1',
+            'building' => 'テストビル101',
+            'image' => null,
+        ]);
     }
 }
