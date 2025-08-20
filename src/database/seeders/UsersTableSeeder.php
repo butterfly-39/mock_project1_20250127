84e73761cc630f6cd586eca25d8d7d27333a4fd1
butterfly <serenitaey39@gmail.com>
2025-08-21 01:08:05 +0900

    ダミーデータの修正

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -15,35 +15,51 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // ユーザーを作成
-        $userId = DB::table('users')->insertGetId([
-            'name' => 'test',
-            'email' => 'test@test.com',
+        // 出品者ユーザー1を作成
+        $seller1Id = DB::table('users')->insertGetId([
+            'name' => '出品者1',
+            'email' => 'test1@test.com',
             'password' => Hash::make('password'),
         ]);
 
-        // ログインユーザー作成
-        $loginUserId = DB::table('users')->insertGetId([
-            'name' => 'test2',
+        // 出品者ユーザー2を作成
+        $seller2Id = DB::table('users')->insertGetId([
+            'name' => '出品者2',
             'email' => 'test2@test.com',
             'password' => Hash::make('password'),
         ]);
 
-        // プロフィールを作成
+        // 購入者ユーザー3を作成
+        $buyerId = DB::table('users')->insertGetId([
+            'name' => '購入者3',
+            'email' => 'test3@test.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // 出品者ユーザー1のプロフィール作成
         DB::table('profiles')->insert([
-            'user_id' => $userId,
+            'user_id' => $seller1Id,
             'postal_code' => '123-4567',
             'address' => '東京都渋谷区代々木1-1-1',
             'building' => 'テストビル101',
             'image' => null,
         ]);
 
-        // ログインユーザープロフィール作成
+        // 出品者ユーザー2のプロフィール作成
         DB::table('profiles')->insert([
-            'user_id' => $loginUserId,
-            'postal_code' => '123-4567',
-            'address' => '東京都渋谷区代々木1-1-1',
-            'building' => 'テストビル101',
+            'user_id' => $seller2Id,
+            'postal_code' => '123-4568',
+            'address' => '東京都渋谷区代々木1-1-2',
+            'building' => 'テストビル102',
+            'image' => null,
+        ]);
+
+        // 購入者ユーザー3のプロフィール作成
+        DB::table('profiles')->insert([
+            'user_id' => $buyerId,
+            'postal_code' => '123-4569',
+            'address' => '東京都渋谷区代々木1-1-3',
+            'building' => 'テストビル103',
             'image' => null,
         ]);
     }
